0378c66ce3c7d512607145cd8d76873570692da9
Rusdi125312 <rusdirustiandi99@gmail.com git config --global user.name>
2026-06-09 08:16:17 +0700

    update kurang

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -3,8 +3,8 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint('https://sgp.cloud.appwrite.io/v1') // URL Appwrite Cloud
-    .setProject('6a26e47900199ad9c2cb'); // Copy dari dashboard Appwrite
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!) 
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
 export const databases = new Databases(client);
 export const account = new Account(client);
